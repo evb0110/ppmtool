18bf1e596f69f6a9b6c6f41f29c8583fac618610
evb0110 <7149553@gmail.com>
2020-03-13 22:50:30 +0300

    initial

---
+++ src/test/java/evb0110/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package evb0110.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
