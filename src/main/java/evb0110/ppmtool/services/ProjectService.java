4f0dd67aefa0ac10f08b3904db31d11ec7e8e610
evb0110 <7149553@gmail.com>
2020-03-16 00:05:49 +0300

    finish first spring part

--- src/main/java/evb0110/ppmtool/services/ProjectService.java
+++ src/main/java/evb0110/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package evb0110.ppmtool.services;
 
 import evb0110.ppmtool.domain.Project;
+import evb0110.ppmtool.exceptions.ProjectIdException;
 import evb0110.ppmtool.repositories.ProjectsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,9 +10,44 @@ import org.springframework.stereotype.Service;
 public class ProjectService {
 
     @Autowired
-    private ProjectsRepository projectsRepository;
+    private ProjectsRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        return projectsRepository.save(project);
+
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already " +
+                    "exists");
+        }
+
+    }
+
+    public Project findProjectByIdentifier(String projectId) {
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null) {
+            throw new ProjectIdException("Project ID '" + projectId.toUpperCase() + "' does not " +
+                    "exist");
+        }
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects() {
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null) {
+            throw new ProjectIdException("Cannot delete Project with ID '" + projectId + "', it does not exist");
+
+        }
+
+        projectRepository.delete(project);
     }
 }
